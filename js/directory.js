685d4b502db59c6e1853220333e2936c7063c981
Andrew Miles <amiles56@gmail.com>
2020-11-11 21:26:24 -0500

    Implemented sticky footer, finished populating default gallery

--- js/directory.js
+++ js/directory.js
@@ -1,38 +1,42 @@
 const items = [{
-    Title: ' Home ',
-    Url: 'index.html',
-    Type: ' Home ',
-    Genre: '',
-    Description: '',
-    Image: '../img/',
-    Alt: ' Click here to view '
+    Title: 'Inferno',
+    Url: 'https://www.gutenberg.org/files/41537/41537-h/41537-h.htm',
+    Type: 'Text',
+    Artist: 'Dante Alighieri',
+    Genre: 'Fantasy',
+    Description: 'The first part of Dantes Divine Comedy',
+    Image: '../img/book',
+    Alt: ' Click here to checkout '
 },
 {
-    Title: ' Home ',
-    Url: 'index.html',
-    Type: ' Home ',
-    Genre: '',
-    Description: '',
-    Image: '../img/',
-    Alt: ' Click here to view '
+    Title: '12th Floor Party',
+    Artist: 'Jules Gaia',
+    Url: 'https://www.epidemicsound.com/track/pN2JH0wO2W/',
+    Type: 'Music',
+    Genre: 'Electroswing',
+    Description: 'A fun, poppy electroswing tune',
+    Image: '../img/album',
+    Alt: ' Click here to checkout '
 },
 {
-    Title: ' Home ',
-    Url: 'index.html',
-    Type: ' Home ',
-    Genre: '',
+    Title: 'The Dunwich Horror',
+    Artist: 'H.P. Lovecraft',
+    Url: 'https://www.gutenberg.org/files/50133/50133-h/50133-h.htm',
+    Type: 'Text',
+    Genre: 'Horror',
     Description: '',
-    Image: '../img/',
-    Alt: ' Click here to view '
+    Image: '../img/book',
+    Alt: ' Click here to checkout '
 },
 {
-    Title: ' Home ',
+    Title: 'Unknown',
+    Artist: 'Unknown',
     Url: 'index.html',
-    Type: ' Home ',
-    Genre: '',
-    Description: '',
-    Image: '../img/',
-    Alt: ' Click here to view '
+    Type: 'Image',
+    Genre: 'Unknown',
+    Description: 'a thing',
+    Image: '../img/image',
+    Alt: ' Click here to checkout '
 }
 
 
